5b52e9e67b2145bfd5c1c525d452d0e648de4757
JK Lacrym0sa <artyom.byzov.00@gmail.com>
2024-05-10 19:01:03 +0300

    Добавьте файлы проекта.

---
+++ Fraction/Fractions.cs
@@ -0,0 +1,43 @@
+﻿
+// задача Класс Дробное число со знаком (Fractions). Число должно быть представлено двумя полями:
+// целая часть - длинное целое со знаком, дробная часть - беззнаковое короткое целое.
+// Реализовать арифметические операции сложения, вычитания, умножения и операции сравнения. В функции main проверить эти методы.
+class Fractions
+{
+    public int Numenator;      // числитель 
+    public int Denumenator;   // знаменатель 
+
+    public Fractions(int Numenator, int Denumenator)
+    {
+        this.Numenator = Numenator;
+        this.Denumenator = Denumenator;
+    }
+
+    public static Fractions Sum(Fractions a, Fractions b)        // сложение 
+    {
+        int resultNum = (a.Numenator * b.Denumenator) + (b.Numenator * a.Denumenator);
+        int resultDenum = a.Denumenator * b.Denumenator;
+
+        return new Fractions(resultNum, resultDenum);
+    }
+    public static Fractions Subtraction(Fractions a, Fractions b)    // вычитание
+    {
+        int resultNum = (a.Numenator * b.Denumenator) - (b.Numenator * a.Denumenator);
+        int resultDenum = a.Denumenator * b.Denumenator;
+
+        return new Fractions(resultNum, resultDenum);
+    }
+    public static Fractions Multiplication(Fractions a, Fractions b)    // умножение
+    {
+        int resultNum = a.Numenator * b.Numenator;
+        int resultDenum = a.Denumenator * b.Denumenator;
+
+        return new Fractions(resultNum, (ushort)resultDenum);
+    }
+    public static bool Compare(Fractions a, Fractions b) //сравнение 
+    {
+        double numA = a.Numenator * b.Denumenator;
+        double numB = b.Numenator * a.Denumenator;
+        return numA == numB;
+    }
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        Fractions frac1 = new Fractions(4, 8);
+        Fractions frac2 = new Fractions(8, 16);
+
+        Fractions sum = Fractions.Sum(frac1, frac2);
+        Fractions sub = Fractions.Subtraction(frac1, frac2);
+        Fractions mult = Fractions.Multiplication(frac1, frac2);
+
+        Console.WriteLine("Fraction 1: {0}.{1}", frac1.Numenator, frac2.Denumenator);
+        Console.WriteLine("Fraction 2: {0}.{1}", frac1.Numenator, frac2.Denumenator);
+        Console.WriteLine("Sum: {0}.{1}", sum.Numenator, sum.Denumenator);
+        Console.WriteLine("Subtraction: {0}.{1}", sub.Numenator, sub.Denumenator);
+        Console.WriteLine("Multiplication: {0}.{1}", mult.Numenator, mult.Denumenator);
+        Console.WriteLine("Compare result: " + Fractions.Compare(frac1, frac2));
+    }
+}
